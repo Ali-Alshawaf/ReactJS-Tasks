daf31172874c6893f17ed26a0aa58d489e24b9a6
alijaleel <alijaleel96@gmail.com>
2025-03-06 10:59:49 +0300

    Remember me

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,23 +3,43 @@ import './LoginForm.css';
 
 export default function LoginForm() {
 
-    
-
     const handleSubmit = (event) => {
         event.preventDefault();
-    
+
         const email = document.getElementById('Email').value.toLowerCase();
         const password = document.getElementById('Password').value;
-    
+
         let users = JSON.parse(localStorage.getItem('users')) || [];
-    
+
         const validUser = users.find(u => u.email === email && u.password === password);
-    
+
         if (validUser) {
             document.getElementById('error').style.display = 'none';
             window.location.href = 'Home';
+            if (document.getElementById('rememberMe').checked) {
+                localStorage.setItem('email', email);
+                localStorage.setItem('password', password);
+            } else {
+                localStorage.removeItem('email');
+                localStorage.removeItem('password');
+            }
         } else {
             document.getElementById('error').style.display = 'block';
+        }
+    };
+
+    window.onload = function() {
+        const emailInput = document.getElementById('Email');
+        const passwordInput = document.getElementById('Password');
+        const rememberMe = document.getElementById('rememberMe');
+
+        const storedEmail = localStorage.getItem('email');
+        const storedPassword = localStorage.getItem('password');
+
+        if (storedEmail && storedPassword) {
+            emailInput.value = storedEmail;
+            passwordInput.value = storedPassword;
+            rememberMe.checked = true;
         }
     };
 
@@ -32,12 +52,17 @@ export default function LoginForm() {
                 <label htmlFor="Password">كلمة المرور</label>
                 <input id="Password" type="password" placeholder="********" required />
                 <p id="error" className="error">البريد الالكتروني أو كلمة المرور غير صحيحة</p>
+
+                <div className="remember-me">
+                    <label htmlFor="rememberMe">تذكرني</label>
+                    <input type="checkbox" id="rememberMe" />
+                </div>
+
                 <input className="submit" type="submit" value="تسجيل الدخول" />
             </form>
             <hr className="Line" />
             <div className="flex-container">
-            <LanguageBtn />
-
+                <LanguageBtn />
                 <div className="Links">
                     <a href="Signup" className="link">تسجيل حساب</a>
                 </div>
